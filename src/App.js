7a76d6c7cc81bda145f0740abff2ebdc4a0f8eee
M. Cullen McClellan <cullenmcclellan@gmail.com>
2020-10-29 20:13:17 -0400

    API Call + Character Component Start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [rickMortyChars, setRickMortyChars] = useState({});
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const getCharsFromRick = (() => {
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setRickMortyChars(response.data.results);
+        console.log(response.data);
+      })
+      .catch(err => { console.log(err) });
+  })
+
+  useEffect(() => {
+    getCharsFromRick();
+  }, [])
+
+  console.log(rickMortyChars);
 
   return (
     <div className="App">
