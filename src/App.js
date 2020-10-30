6f6a1338a6f49e80cb75721d1f83a8e26a7cc063
M. Cullen McClellan <cullenmcclellan@gmail.com>
2020-10-30 01:17:13 -0400

    MVP Met

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Character from './components/Character'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [rickMortyChars, setRickMortyChars] = useState({});
+  const [rickMortyChars, setRickMortyChars] = useState(null);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -14,8 +14,8 @@ const App = () => {
   const getCharsFromRick = (() => {
     axios.get(`https://rickandmortyapi.com/api/character/`)
       .then(response => {
-        setRickMortyChars(response.data.results);
-        console.log(response.data);
+        setRickMortyChars(Object.values(response.data.results));
+        console.log(Object.values(response.data.results));
       })
       .catch(err => { console.log(err) });
   })
@@ -25,12 +25,20 @@ const App = () => {
   }, [])
 
   console.log(rickMortyChars);
-
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+  if (rickMortyChars === null) {
+    return (
+      <h2>LOADING...</h2>
+    )
+  }
+  else {
+    return (
+      <div className="App">
+        <img alt={'logo'} src={require('./images/logo.png')}></img>
+        <h1 className="Header">Characters</h1>
+        {rickMortyChars.map((char, index )=> 
+        { return <Character key={index} data={char} /> })}
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,63 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharCard = styled.section`
+    display: flex;
+    background: #04b0c8;
+    width: 50em;
+    margin: 2em auto;
+    border: 30px solid #04b0c8;
+    border-radius: 30px;
+`;
+
+const ImgDiv = styled.div`
+    width: 25rem;
+    margin-left: 0;
+    text-align: left;
+`
+
+const CharImg = styled.img`
+    border: 3px solid #124d4f;
+    border-radius: 30px;
+`;
+
+const AttrDiv = styled.div`
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    margin: auto 2rem auto 0;
+    text-align: left;
+`;
+
+const Name = styled.h2`
+    font-size: 2.5rem;
+    margin: 0;
+`;
+
+export default function Character(props) {
+    const { data } = props;
+    if (data === null) {
+        return (
+            <h2>LOADING...</h2>
+        )
+    }
+    else {
+        return(
+            <CharCard>
+                <ImgDiv>
+                    <CharImg src={data.image}></CharImg>
+                </ImgDiv>
+                <AttrDiv>
+                    <Name>{data.name}</Name>
+                    <h3>{`Species: ${data.species}`}</h3>
+                    <h3>{`Origin: ${data.origin.name}`}</h3>
+                    <h3>{`Location: ${data.location.name}`}</h3>
+                    <h3>{`Number of Episodes: ${data.episode.length}`}</h3>
+                    <h3>{`Status: ${data.status}`}</h3>
+                </AttrDiv>
+            </CharCard>
+        )
+        
+    }
+}
